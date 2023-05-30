2e9d8a45d9cd8f74848505ce96a3d03fbed02a96
Anjit Silwal <a10.silwal@gmail.com>
2023-05-30 17:12:33 +0545

    second commit

---
+++ A00-MyFirstSB/src/main/java/com/example/MyFirstSB/TestController.java
@@ -0,0 +1,15 @@
+package com.example.MyFirstSB;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class TestController {
+	@RequestMapping("/")
+	public String firstHandler() {
+		return "Testing";
+	}
+
+}
---
+++ A00-MyFirstSB/src/test/java/com/example/MyFirstSB/MyFirstSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyFirstSB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
